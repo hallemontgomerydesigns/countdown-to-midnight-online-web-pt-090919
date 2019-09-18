54fea7ddb5d05188f76c1364ccfd8d64dc394857
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-18 02:32:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
+
 #write your code here
 
-def countdown
+def countdown (seconds_to_midnight)
+  
+   while seconds_to_midnight > 0
+  puts "#{seconds_to_midnight} SECONDS!"
+  seconds_to_midnight -= 1
+end
+
+"HAPPY NEW YEAR!"
+  
+end
+
+def countdown_with_sleep (seconds_to_midnight)
+  while seconds_to_midnight > 0
+  puts "#{seconds_to_midnight} SECONDS!"
+  sleep(1)
+  seconds_to_midnight -= 1
+end
+"HAPPY NEW YEAR!"
 end
